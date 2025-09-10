158dc263bf1fdab2c377596f36d0abc01c67533e
baont-runsystem <baont1@runsystem.net>
2025-09-10 10:34:19 +0700

    update page

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function sayHello() {
-    const messageEl = document.getElementById('message');
-    messageEl.textContent = "Cảm ơn bạn đã nhấn vào! ✨";
+window.addEventListener("scroll", () => {
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+let current = "";
+
+
+sections.forEach((sec) => {
+const top = window.scrollY;
+const offset = sec.offsetTop - 80;
+const height = sec.offsetHeight;
+if (top >= offset && top < offset + height) {
+current = sec.getAttribute("id");
 }
+});
+
+
+navLinks.forEach((a) => {
+a.classList.remove("text-blue-600", "font-bold");
+if (a.getAttribute("href") === `#${current}`) {
+a.classList.add("text-blue-600", "font-bold");
+}
+});
+});
